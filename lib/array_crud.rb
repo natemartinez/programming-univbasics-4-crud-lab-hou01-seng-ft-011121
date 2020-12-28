559db665a2b347b20c2130edefa8eddb4ddb9817
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-28 02:29:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,8 +31,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array=["#{array}","#{array}"]
-  
+  array=["#{array}","#{arr"]
   retrieve_element_from_index[1]
 end
 
