08bf78d45f08069400f36df32ec373043427b547
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-28 02:03:49 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array=["#{array}","#{array}"]
-  array.pop("arrays!")
+  array.pop("#{array}")
 end
 
 def remove_element_from_start_of_array(array)
