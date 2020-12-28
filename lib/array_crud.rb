3ca84e730f704dd892aac398ae9ee80beb893145
Nathaniel Martinez <nathanielmartinez2001@gmail.com>
2020-12-28 02:56:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
+  array =["wow", "am"]
   
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
